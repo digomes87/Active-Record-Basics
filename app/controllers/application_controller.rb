159b7bc1a9aa6d190f98f5c778dc46d75e418604
yochiwarez <yochiwarez@gmail.com>
2019-11-11 17:14:25 -0500

    login controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  def home
+    render 'home/index'
+  end
 end
---
+++ app/controllers/logins_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class LoginsController < ApplicationController
+  def create
+    @author = Author.find_by_email(params[:email])
+    if @author != nil
+      session[:user_id] = if @author.password == params[:password]
+                            Author.find_by_email(params[:email]).id
+                          end
+    end
+    redirect_to('/posts')
+  end
+end
